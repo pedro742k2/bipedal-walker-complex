02e2b63722f1e47f8a1fe9c35c59a6df1bdeed11
Pedro Batista <pmpb742k2@gmail.com>
2024-07-07 10:02:56 +0100

    checkpoints

--- OLD_SCRIPTS/main.py
+++ OLD_SCRIPTS/main.py
@@ -1,6 +1,5 @@
 import argparse
 import logging
-import sys
 from matplotlib import pyplot as plt
 import pybullet as p
 import time
@@ -126,11 +125,9 @@ joint_ids = [i for i in range(num_joints)]
 joints_max_limit = []
 joints_min_limit = []
 
-
-MAX_TORQUE_FORCE = 700
-OBSERVATION_SPACE_DIM = 49
-ACTION_SPACE_DIM = num_joints
-last_action = [0 for _ in range(ACTION_SPACE_DIM)]
+MAX_TORQUE_FORCE = 1_000
+OBSERVATION_SPACE_DIM = 101
+ACTION_SPACE_DIM = num_joints * 2
 print(f"""Robot num joints: {num_joints} | State space dim: {
       OBSERVATION_SPACE_DIM} | Action space dim: {ACTION_SPACE_DIM}""")
 
@@ -153,20 +150,14 @@ agent = Agent(start_steps=warmup, action_space_dims=ACTION_SPACE_DIM,
               state_space_dims=OBSERVATION_SPACE_DIM, actor_lr=actor_lr, critic_lr=critic_lr, batch_size=batch_size, gamma=gamma, max_action=MAX_ACTION_VALUE, min_action=MIN_ACTION_VALUE, max_replay_size=memory_size, tau=tau, alpha=0.2, runtest_folder_name=runtest_folder_name)
 
 
-def update_last_action(action):
-    global last_action
-    last_action = action
-
-
 def reset_env():
-    global last_action
     # p.setJointMotorControlArray(robot_id, [i for i in range(
     #     num_joints)], p.POSITION_CONTROL, targetPositions=[0 for _ in range(num_joints)])
     p.resetBasePositionAndOrientation(
         robot_id, cubeStartPos, cubeStartOrientation)
     for i in range(num_joints):
         p.resetJointState(robot_id, i, targetValue=0, targetVelocity=0)
-    update_last_action([0 for _ in range(ACTION_SPACE_DIM)])
+    update_last_ts_robot_on_ground_flag(False)
 
 
 def scale_value(x, lower_limit, upper_limit):
@@ -190,16 +181,29 @@ def is_state_success():
     return (abs(x - X_TARGET) < 0.05 and abs(y-Y_TARGET) < 0.05)
 
 
-# def get_relative_distance_from_target(current_x, current_y):
-#     X_TARGET, Y_TARGET = TARGET_POSITION
+def get_relative_distance_from_target(current_x, current_y):
+    X_TARGET, Y_TARGET = TARGET_POSITION
 
-#     return [abs(X_TARGET-current_x), abs(Y_TARGET-current_y)]
-
-
-def euclidean_distance(point1, point2):
-    return np.linalg.norm(np.array(point1) - np.array(point2))
+    return [abs(X_TARGET-current_x), abs(Y_TARGET-current_y)]
 
 
+# TODO: Big base robot
+# def get_contact_sensor_values():
+#     heel_1_contact = min(len(p.getContactPoints(
+#         robot_id, plane_id, linkIndexA=3)), 1)
+#     foot_1_contact = min(len(p.getContactPoints(
+#         robot_id, plane_id, linkIndexA=4)), 1)
+
+#     heel_2_contact = min(len(p.getContactPoints(
+#         robot_id, plane_id, linkIndexA=8)), 1)
+#     foot_2_contact = min(len(p.getContactPoints(
+#         robot_id, plane_id, linkIndexA=9)), 1)
+
+#     return [
+#         max(foot_1_contact, heel_1_contact),
+#         max(foot_2_contact, heel_2_contact)
+#     ]
+# TODO: SMALL BASE ROBOT
 def get_contact_sensor_values():
     foot_1_contact = min(len(p.getContactPoints(
         robot_id, plane_id, linkIndexA=4)), 1)
@@ -214,7 +218,7 @@ def get_contact_sensor_values():
 
 
 def is_state_truncated(timestep):
-    return timestep == timesteps-1
+    return timestep == TIMESTEPS_PER_EPOCH-1
 
 
 def get_distance_from_target_diff(state, new_state):
@@ -236,23 +240,20 @@ def get_distance_from_target_diff(state, new_state):
     return distance_from_targets_diff
 
 
-def get_center_of_mass():
-    dynamics_info = p.getDynamicsInfo(robot_id, -1)
-    # Center of mass position in local coordinates
-    com_position = dynamics_info[3]
-    base_position, base_orientation = p.getBasePositionAndOrientation(robot_id)
-
-    com_world = p.multiplyTransforms(
-        base_position, base_orientation, com_position, (0, 0, 0, 1)
-    )[0]
-
-    return com_world
-
-
 def get_current_observation():
     observation = np.array([])
 
     for joint_id in range(num_joints):
+        joint_info = p.getLinkState(robot_id, joint_id)
+
+        # Append joint position to observation (10 motors * 3 position dimensions = 30 items]
+        joint_3d_position = joint_info[0]
+        observation = np.append(observation, joint_3d_position)
+
+        # Append joint orientation to observation (10 motors * 4 position dimensions = 40 items]
+        joint_orientation = joint_info[1]
+        observation = np.append(observation, joint_orientation)
+
         # Append joint rotation and velocity to observation (10 motors * 2 states = 20 items)
         joint_rotation, joint_velocity, _, _ = p.getJointState(
             robot_id, joint_id)
@@ -264,27 +265,19 @@ def get_current_observation():
     observation = np.append(
         observation, [robot_linear_velocity, robot_angular_velocity])
 
-    # Append current x, y coordinates and height (z)
-    (current_x, current_y, height), orientation = p.getBasePositionAndOrientation(robot_id)
-    observation = np.append(observation, [current_x, current_y, height])
-
-    # Append euclidian distance from target (1 item)
+    # Append relative target position (|x-target|, |y-target|) (2 items)
+    (current_x, current_y, _), _ = p.getBasePositionAndOrientation(robot_id)
     observation = np.append(
         observation,
-        euclidean_distance([current_x, current_y], TARGET_POSITION)
+        get_relative_distance_from_target(
+            current_x, current_y)
     )
-
-    # Append center of mass
-    observation = np.append(observation, get_center_of_mass())
-
-    # Append orientation
-    observation = np.append(observation, orientation)
 
     # Append contact sensors (binary array with two elements: feet touching the ground) (2 items)
     observation = np.append(observation, get_contact_sensor_values())
 
-    # Append last action
-    observation = np.append(observation, last_action)
+    # Append robot fell state (1 item)
+    observation = np.append(observation, get_robot_fell_state())
 
     # Total of 101 elements
     return observation
@@ -294,45 +287,97 @@ def is_robot_on_ground():
     return len(p.getContactPoints(robot_id, plane_id, linkIndexA=-1)) > 0
 
 
+# Indicates if the robot, in the last timestep, was on the ground
+last_ts_robot_on_ground_flag = False
+
+
+def update_last_ts_robot_on_ground_flag(flag: bool):
+    global last_ts_robot_on_ground_flag
+    last_ts_robot_on_ground_flag = flag
+
+
+def get_robot_fell_state():
+    return last_ts_robot_on_ground_flag
+
+
+def is_robot_up() -> bool:
+    (_, _, base_height), _ = p.getBasePositionAndOrientation(robot_id)
+    return base_height > 0.8
+
+
+def is_robot_on_ground() -> bool:
+    (_, _, base_height), _ = p.getBasePositionAndOrientation(robot_id)
+    return base_height < 0.4
+
+
+def robot_fell():
+    return (not last_ts_robot_on_ground_flag and is_robot_on_ground())
+
+
+def recovered_from_fall():
+    return (last_ts_robot_on_ground_flag and is_robot_up())
+
+
 def can_plot_rewards(current_epoch) -> bool:
-    return current_epoch % 100 == 0
+    return current_epoch % 50 == 0
+
+
+def making_progress_towards_getting_up(past_position, new_position):
+    past_height = past_position[2]
+    new_height = new_position[2]
+    return new_height - past_height > 0.01
 
 
 rewards_data_plot = []
 
 
-def get_robot_height():
-    (_, _, base_height), _ = p.getBasePositionAndOrientation(robot_id)
-    return base_height
-
-
-def get_reward(past_position, new_position, epoch):
+def get_reward(past_position, new_position, forces_applied, epoch):
     timestep_reward = 0
 
-    # Rewarded for keeping base high
-    robot_height_reward = get_robot_height()-0.5
-    timestep_reward += robot_height_reward
-
-    robot_fell_penalty = -300 if is_robot_on_ground() else 0
-    # robot_recovered_reward = 20 if recovered_from_fall() else 0
+    robot_fell_penalty = -15 if robot_fell() else 0
+    robot_recovered_reward = 20 if recovered_from_fall() else 0
 
     timestep_reward += robot_fell_penalty
-    # timestep_reward += robot_recovered_reward
+    timestep_reward += robot_recovered_reward
+
+    if is_robot_up():
+        update_last_ts_robot_on_ground_flag(False)
+    elif is_robot_on_ground():
+        update_last_ts_robot_on_ground_flag(True)
+    # Else doesnt update
+
+    global_robot_fell_state = get_robot_fell_state()
+
+    force_penalty = 0
+    if not global_robot_fell_state:
+        force_penalty = 0 - (np.array(forces_applied)/MAX_TORQUE_FORCE).mean()
+    timestep_reward += force_penalty
 
     (x_speed, y_speed, _), _ = p.getBaseVelocity(robot_id)
 
-    # speed = 0
-    # if global_robot_fell_state:
-    #     speed = np.sqrt(np.square(x_speed)+np.square(y_speed)) * 1e-1
-    # elif not global_robot_fell_state:
-    speed = np.sqrt(np.square(x_speed)+np.square(y_speed)) * 1e-2
+    speed = 0
+    if global_robot_fell_state:
+        speed = np.sqrt(np.square(x_speed)+np.square(y_speed)) * 1e-4
+    elif not global_robot_fell_state:
+        speed = np.sqrt(np.square(x_speed)+np.square(y_speed)) * 1e-3
     timestep_reward += speed
+
+    robot_on_ground_continuous_penalty = -0.5 if global_robot_fell_state else 0.25
+
+    timestep_reward += robot_on_ground_continuous_penalty
+
+    making_progress_from_fall = 0
+
+    if global_robot_fell_state and making_progress_towards_getting_up(past_position, new_position):
+        making_progress_from_fall = 5  # Reward for partial progress at getting up\
+
+    timestep_reward += making_progress_from_fall
 
     both_feet_touching_ground_reward = 0
 
     foot_contact_readings = get_contact_sensor_values()
     # If both feet touching the ground, receive a reward
-    if foot_contact_readings[0] and foot_contact_readings[1]:
+    if not global_robot_fell_state and foot_contact_readings[0] and foot_contact_readings[1]:
         both_feet_touching_ground_reward = 0.1
 
     timestep_reward += both_feet_touching_ground_reward
@@ -342,8 +387,12 @@ def get_reward(past_position, new_position, epoch):
 
     relative_distance_from_target = 0
 
-    relative_distance_from_target = get_distance_from_target_diff(
-        past_position, new_position) * 1e3
+    # if global_robot_fell_state:
+    #     relative_distance_from_target = get_distance_from_target_diff(
+    #         past_position, new_position)
+    if not global_robot_fell_state:
+        relative_distance_from_target = get_distance_from_target_diff(
+            past_position, new_position) * 1e3
 
     relative_distance_from_target = max(-0.2, relative_distance_from_target)
 
@@ -351,10 +400,13 @@ def get_reward(past_position, new_position, epoch):
 
     if can_plot_rewards(epoch):
         rewards_data_plot.append({
-            "robot_height_reward": robot_height_reward,
+            "force_penalty": force_penalty,
             "speed": speed,
             "robot_fell_penalty": robot_fell_penalty,
+            "robot_recovered_reward": robot_recovered_reward,
+            "robot_on_ground_continuous_penalty": robot_on_ground_continuous_penalty,
             "relative_distance_from_target": relative_distance_from_target,
+            "making_progress_towards_getting_up": making_progress_from_fall,
             "foot_contact_readings": both_feet_touching_ground_reward,
             "success_reward": 300 if is_state_success() else 0,
             "total_reward": timestep_reward
@@ -377,7 +429,7 @@ for epoch in range(TOTAL_EPOCHS):
     logger.info(f"\t- Start date:\t{epoch_start_timestamp}")
     log_divider()
 
-    for timestep in range(timesteps):
+    for timestep in range(TIMESTEPS_PER_EPOCH):
         position, _ = p.getBasePositionAndOrientation(robot_id)
         p.resetDebugVisualizerCamera(3, 45, -25, position)
 
@@ -389,14 +441,28 @@ for epoch in range(TOTAL_EPOCHS):
 
         # Get actions | range: [-1, 1]
         actions = agent.select_actions(current_observation, add_noise)
-        update_last_action(actions)
 
-        rescaled_position_actions = rescale_actions(actions)
+        position_actions = actions[:10]
+        force_actions = actions[10:]
+
+        rescaled_position_actions = rescale_actions(position_actions)
+        rescaled_force_actions = [
+            ((force+1)/2)*MAX_TORQUE_FORCE for force in force_actions]
+
+        # print("Rescaled position actions:")
+        # for a in rescaled_position_actions:
+        #     print(f"\t - {a}")
+
+        # print("Rescaled force actions:")
+        # for a in rescaled_force_actions:
+        #     print(f"\t - {a}")
+
+        # print("-."*50)
 
         # Apply actions
         for i in joint_ids:
             p.setJointMotorControl2(robot_id, i, controlMode=p.POSITION_CONTROL,
-                                    targetPosition=rescaled_position_actions[i], maxVelocity=100, force=MAX_TORQUE_FORCE)
+                                    targetPosition=rescaled_position_actions[i], maxVelocity=100, force=rescaled_force_actions[i])
 
         # Perform actions in the environment
         p.stepSimulation()
@@ -410,43 +476,51 @@ for epoch in range(TOTAL_EPOCHS):
         # Get new robot positions
         new_position, _ = p.getBasePositionAndOrientation(robot_id)
 
-        # Check if epoch truncated
-        terminal_flag = is_robot_on_ground() or is_state_truncated(timestep)
-
         # Get timestep reward
-        reward = get_reward(position, new_position, epoch)
+        reward = get_reward(position, new_position,
+                            rescaled_force_actions, epoch)
 
         # Add reward to epoch accumulated score
         epoch_acc_score += reward
+
+        # Check if epoch truncated
+        terminal_flag = robot_fell()
 
         agent.remember(current_observation, actions, reward,
                        new_observation, terminal_flag)
 
         agent.learn()
 
-        if is_state_success() or terminal_flag:
+        if is_state_success():
             break
 
     if can_plot_rewards(epoch):
         x = [i for i in range(len(rewards_data_plot))]
         plt.figure(figsize=(12, 8), dpi=200)
         plt.title(f"Rewards plot from epoch {epoch}")
-
+        plt.plot(x, [data["force_penalty"] for data in rewards_data_plot])
         plt.plot(x, [data["speed"] for data in rewards_data_plot])
         plt.plot(x, [data["robot_fell_penalty"] for data in rewards_data_plot])
-        plt.plot(x, [data["robot_height_reward"]
+        plt.plot(x, [data["robot_recovered_reward"]
                  for data in rewards_data_plot])
+        plt.plot(x, [data["robot_on_ground_continuous_penalty"]
+                     for data in rewards_data_plot])
         plt.plot(x, [data["relative_distance_from_target"]
+                     for data in rewards_data_plot])
+        plt.plot(x, [data["making_progress_towards_getting_up"]
                      for data in rewards_data_plot])
         plt.plot(x, [data["foot_contact_readings"]
                      for data in rewards_data_plot])
         plt.plot(x, [data["success_reward"]
                      for data in rewards_data_plot])
         plt.plot(x, [data["total_reward"]
-                     for data in rewards_data_plot], color="black")
-
+                     for data in rewards_data_plot], ".", color="black", markersize=0.5)
         plt.legend([
-            "speed", "robot_fell_penalty", "robot_height_reward", "relative_distance_from_target", "foot_contact_readings",    "success_reward", "total_reward"])
+            "torque_force_penalty",
+            "speed", "robot_fell_penalty", "robot_recovered_reward",
+            "robot_on_ground_continuous_penalty", "relative_distance_from_target", "making_progress_towards_getting_up",
+            "foot_contact_readings",
+            "success_reward", "total_reward"])
         plt.savefig(
             f"{runtest_folder_name}/plots/rewards_epoch_{epoch}.png", dpi=300)
         plt.ylim(top=1, bottom=-1)
